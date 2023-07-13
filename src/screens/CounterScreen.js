fa14a6580f3fd527cd111888211cec0f817e59a6
veyselguven <vgbasbaydar@gmail.com>
2023-07-13 14:17:19 -0700

    testing2

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,32 @@
 import { View, Text, StyleSheet, Button } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <View>
-      <Text>Current Count;</Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          setCounter((prev) => prev + 1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter((prev) => prev - 1);
+        }}
+      />
+      <Text style={styles.textStyle}>Current Count; {counter}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 20,
+    textAlign: "center",
+  },
+});
 
 export default CounterScreen;
