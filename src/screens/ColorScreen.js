4e6872800fabce2dca5af85c5554c60783accf22
veyselguven <vgbasbaydar@gmail.com>
2023-07-13 17:18:30 -0700

    color functionality added

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,14 +1,45 @@
-import { View, Text, Button, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, Button, StyleSheet, FlatList } from "react-native";
+import React, { useState } from "react";
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+  //   console.log(colors);
   return (
     <View>
-      <Button title="Add a Color" />
+      <Button
+        title="Add a Color"
+        onPress={() => {
+          setColors((prev) => {
+            return [...prev, randomRgb()];
+          });
+        }}
+      />
+
+      <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                height: 100,
+                width: 100,
+                backgroundColor: item,
+              }}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
 
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red},${green},${blue})`;
+};
 const styles = StyleSheet.create({});
 
 export default ColorScreen;
