89375a48c633aececfdb1b922ad601e2bc4e832f
veyselguven <vgbasbaydar@gmail.com>
2023-07-13 22:41:35 -0700

    added text router

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -30,6 +30,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Square Demo"
         onPress={() => navigation.navigate("Square")}
       />
+      <Button
+        title="Go to Text Demo"
+        onPress={() => navigation.navigate("Text")}
+      />
       {/* <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
         <Text>Go To List Demo</Text>
       </TouchableOpacity> */}
